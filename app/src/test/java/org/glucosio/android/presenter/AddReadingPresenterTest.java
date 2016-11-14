69a4086858711bfcb6010e468490260e73d60788
SoHyang.Jeon <SoHyang.Jeon126@gmail.com>
2016-11-15 01:58:40 +0900

    add Testcode in AddReadingPresenterTest.java

--- app/src/test/java/org/glucosio/android/presenter/AddReadingPresenterTest.java
+++ app/src/test/java/org/glucosio/android/presenter/AddReadingPresenterTest.java
@@ -8,23 +8,16 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.concurrent.ExecutionException;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
-
 
 @RunWith(MockitoJUnitRunner.class)
 public class AddReadingPresenterTest {
@@ -35,31 +28,54 @@ public class AddReadingPresenterTest {
     @InjectMocks
     private AddReadingPresenter presenter;
 
-    @Mock
-    private SplitDateTime splitDateTime;
-
-    @Mock Calendar cal;
-    @Mock Date date;
-
     @Before
     public void setUp() {
         initMocks(this);
     }
 
-
     @Test
-    public void SouldReturnFalseFromReading_WhenCorrectDateInput() throws Exception{
+    public void SouldReturnTrueFromReading_WhenCorrectDateInput() throws Exception{
         assertTrue(presenter.validateDate(TEST_DATE_VALUE));
     }
     @Test
-    public void SouldReturnFalseFromReading_WhenCorrectTimeInput() throws Exception{
+    public void SouldReturnTrueFromReading_WhenCorrectTimeInput() throws Exception{
         assertTrue(presenter.validateTime(TEST_TIME_VALUE));
     }
     @Test
-    public void SouldReturnFalseFromReading_WhenCorrectTextInput() throws Exception{
+    public void SouldReturnTrueFromReading_WhenCorrectTextInput() throws Exception{
         assertTrue(presenter.validateText(TEST_TEXT_VALUE));
     }
 
+    @Test
+    public void ShouldUpdateAllValues_WhenUpdateDate() throws Exception {
+        String mdate = "2016/01/01 01:00:00";
+        Date testreadingDate = new Date(mdate);
+        presenter.updateReadingSplitDateTime(testreadingDate);
 
+        assertThat(presenter.getReadingYear()).isEqualTo("2016");
+        assertThat(presenter.getReadingMonth()).isEqualTo("01");
+        assertThat(presenter.getReadingTime()).isAfterOrEqualsTo("2016-01-01T01:00:00");
+    }
 
+    @Test
+    public void ShouldSetAllValues_WhenSetDate() throws Exception {
+        DateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        Date formatted = Calendar.getInstance().getTime();
+
+        SplitDateTime addSplitDateTime = new SplitDateTime(formatted, inputFormat);
+        presenter.setReadingTimeNow();
+
+        assertThat(presenter.getReadingYear()).isEqualTo(addSplitDateTime.getYear());
+        assertThat(presenter.getReadingMonth()).isEqualTo(addSplitDateTime.getMonth());
+    }
+
+    @Test
+    public void ShouldReturnCorrectTime_WhenRungetReadingCal() throws Exception{
+        String mdate = "2016/01/01 01:00:00";
+        Date testreadingDate = new Date(mdate);
+        presenter.updateReadingSplitDateTime(testreadingDate);
+
+        Calendar cal = presenter.getReadingCal();
+        assertThat(cal.getTime()).isAfterOrEqualsTo("2016-01-01T01:00:00");
+    }
 }
